fcb9b3a861e5a236f766f24f831ccc1ff69c8d70
PEDRO SAMUEL GARCIA <garciape@ar.ibm.com>
2017-01-16 13:52:15 -0600

    Add arraylists.

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.*;
+
 /**
  * Servlet implementation class SimpleServlet
  */
@@ -21,7 +23,36 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        
+        // response.getWriter().print("Hello World!");
+        
+        ArrayList<String> sports = new ArrayList<String>();
+	  	sports.add("Football");
+   		 sports.add("Boxing");
+
+		for(String sport : sports) {
+     			response.getWriter().print(sport); 
+     			response.getWriter().write("<br/>");
+     			
+		}
+		
+			//Major cities and the year they were founded
+		HashMap<String, Integer> majorCities = new HashMap<String, Integer>();
+
+		majorCities.put("New York", 1624);
+		majorCities.put("London", 43);
+		majorCities.put("Mexico City", 1521);
+		majorCities.put("Sao Paulo", 1554);
+
+
+		for (String city : majorCities.keySet() ) {
+
+			response.getWriter().print(city + " was founded in " + majorCities.get(city));
+			response.getWriter().write("<br/>");
+     		
+
+		}
+        
     }
 
 }
